982cd08e30c07a5e1ccf7896f43b14215e3185b8
Angel Irangabiye <angel.irangabiyr@stu.ocsb.ca>
2025-02-20 07:04:19 +0000

    Added Basic_math.py

---
+++ Basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env.python3.
+# Created By: Angel
+# Date: 14.02,2025
+# This program does basic math.
+
+
+def main():
+    print("2+3={}".format(2 + 3))
+    print("5-4={}".format(5 - 4))
+    print("8/3={}".format(8 / 3))
+    print("8/3={}".format(8 / 3))
+    print("3^2={}".format(3 ^ 2))  # bitwise operator
+    print("16**0.5={}".format(16**0.5))
+
+
+if __name__ == "__main__":
+    main()
